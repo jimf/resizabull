5a381edd0e9ecf62cd4eeedd1bf0268445d7e70c
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2016-01-13 21:23:42 -0500

    Add source documentation

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var store = new Store({ window: window, document: document });
 store.bindEvents();
 store.render();
 
+/**
+ * Public-facing resizabull factory method. Instantiates a resizer, registers
+ * it with the store, binds events, and returns the instance.
+ *
+ * @param {HTMLElement} el Element to make resizable
+ * @param {object} [options] Configuration options
+ */
 function resizabull(el, options) {
     var resizer = new Resizer(el, options);
     store.add(resizer);
